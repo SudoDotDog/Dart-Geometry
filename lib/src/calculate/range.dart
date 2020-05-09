29cab34dc12e5133aaa2fb27b9d7c39db23ccffb
WMXPY <wm@mengw.io>
2020-05-09 14:21:41 -0500

    add get linear range by cooridiantes function

--- lib/src/calculate/range.dart
+++ lib/src/calculate/range.dart
@@ -1,1 +1,24 @@
+import 'dart:math';
 
+import 'package:geometry/geometry.dart';
+
+CoordinateRange getLinearRangeByCoordinates(List<Coordinate> coordinates) {
+  double maxLatitude = -double.maxFinite;
+  double minLatitude = double.maxFinite;
+  double maxLongitude = -double.maxFinite;
+  double minLongitude = double.maxFinite;
+
+  for (final Coordinate coordinate in coordinates) {
+    maxLatitude = max(maxLatitude, coordinate.latitude);
+    minLatitude = min(minLatitude, coordinate.latitude);
+    maxLongitude = max(maxLongitude, coordinate.longitude);
+    minLongitude = min(minLongitude, coordinate.longitude);
+  }
+
+  return CoordinateRange(
+    maxLatitude: maxLatitude,
+    minLatitude: minLatitude,
+    maxLongitude: maxLongitude,
+    minLongitude: minLongitude,
+  );
+}
