7f8e29d56ed2fe8fc0c61faefc6f8dd0d40b9494
WMXPY <wm@mengw.io>
2020-05-10 14:55:15 -0500

    init, import

--- lib/coordinate.dart
+++ lib/coordinate.dart
@@ -1,5 +1,7 @@
 library coordinate;
 
+export "./src/arrange/coordinate.dart";
+export "./src/calculate/distance.dart";
 export "./src/calculate/radius.dart";
 export "./src/calculate/range.dart";
 export "./src/entity/entity.dart";
