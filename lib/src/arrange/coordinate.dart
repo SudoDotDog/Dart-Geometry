5d996846b8d4c37d25bab3964817680ae64cf447
WMXPY <wm@mengw.io>
2020-05-10 14:53:40 -0500

    init coordinate

---
+++ lib/src/arrange/coordinate.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:coordinate/coordinate.dart';
+
+List<Coordinate> arrangeCoordinateByLinearDistance(
+    Coordinate start, List<Coordinate> destinations) {
+  final Set<Coordinate> destinationsSet = new Set();
+
+  for (final Coordinate destination in destinations) {
+    destinationsSet.add(destination);
+  }
+
+  final List<Coordinate> result = [];
+
+  while (destinationsSet.length > 0) {}
+}
