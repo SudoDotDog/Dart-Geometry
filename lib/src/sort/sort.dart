01aa61f56bbcb9944fbefb802907a25559abe99f
WMXPY <wm@mengw.io>
2020-05-11 14:33:10 -0500

    implement sort with cache function

--- lib/src/sort/sort.dart
+++ lib/src/sort/sort.dart
@@ -45,3 +45,55 @@ List<T> sortObjectByLinearDistance<T>(
 
   return clone;
 }
+
+List<Coordinate> sortCoordinateByLinearDistanceWithCache(
+  Coordinate start,
+  List<Coordinate> destinations,
+) {
+  final List<Coordinate> clone = destinations.toList();
+  final Map<String, double> cacheMap = Map();
+
+  clone.sort((Coordinate a, Coordinate b) {
+    final double aDistance =
+        calculateLinearDistanceWithCache(cacheMap, start, a);
+    final double bDistance =
+        calculateLinearDistanceWithCache(cacheMap, start, b);
+
+    if (aDistance > bDistance) {
+      return 1;
+    } else if (aDistance < bDistance) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return clone;
+}
+
+List<T> sortObjectByLinearDistanceWithCache<T>(
+  Coordinate start,
+  List<T> destinations,
+  GetCoordinateFunction<T> getCoordinateFunction,
+) {
+  final List<T> clone = destinations.toList();
+  final Map<String, double> cacheMap = Map();
+
+  clone.sort((T a, T b) {
+    final Coordinate aCoordinate = getCoordinateFunction(a);
+    final Coordinate bCoordinate = getCoordinateFunction(b);
+
+    final double aDistance =
+        calculateLinearDistanceWithCache(cacheMap, start, aCoordinate);
+    final double bDistance =
+        calculateLinearDistanceWithCache(cacheMap, start, bCoordinate);
+
+    if (aDistance > bDistance) {
+      return 1;
+    } else if (aDistance < bDistance) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return clone;
+}
