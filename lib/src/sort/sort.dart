3254d62f184d66316056cdc01f58a137b4947798
WMXPY <wm@mengw.io>
2020-05-11 14:28:42 -0500

    implement sort object

--- lib/src/sort/sort.dart
+++ lib/src/sort/sort.dart
@@ -1,12 +1,39 @@
 import 'package:coordinate/coordinate.dart';
 
 List<Coordinate> sortCoordinateByLinearDistance(
-    Coordinate start, List<Coordinate> destinations) {
+  Coordinate start,
+  List<Coordinate> destinations,
+) {
   final List<Coordinate> clone = destinations.toList();
 
   clone.sort((Coordinate a, Coordinate b) {
     final double aDistance = calculateLinearDistance(start, a);
     final double bDistance = calculateLinearDistance(start, b);
+
+    if (aDistance > bDistance) {
+      return 1;
+    } else if (aDistance < bDistance) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return clone;
+}
+
+List<T> sortObjectByLinearDistance<T>(
+  Coordinate start,
+  List<T> destinations,
+  GetCoordinateFunction<T> getCoordinateFunction,
+) {
+  final List<T> clone = destinations.toList();
+
+  clone.sort((T a, T b) {
+    final Coordinate aCoordinate = getCoordinateFunction(a);
+    final Coordinate bCoordinate = getCoordinateFunction(b);
+
+    final double aDistance = calculateLinearDistance(start, aCoordinate);
+    final double bDistance = calculateLinearDistance(start, bCoordinate);
 
     if (aDistance > bDistance) {
       return 1;
--- test/sort/sort_test.dart
+++ test/sort/sort_test.dart
@@ -1,6 +1,8 @@
 import 'package:coordinate/coordinate.dart';
 import 'package:test/test.dart';
 
+import '../mock/object.dart';
+
 void main() {
   group('Sort Coordinate', () {
     test('test sort coordinate', () {
@@ -16,6 +18,31 @@ void main() {
       final List<Coordinate> result = sortCoordinateByLinearDistance(
         start,
         destinations,
+      );
+
+      expect(result[0], equals(destinations[2]));
+      expect(result[1], equals(destinations[4]));
+      expect(result[2], equals(destinations[0]));
+      expect(result[3], equals(destinations[3]));
+      expect(result[4], equals(destinations[1]));
+    });
+  });
+
+  group('Sort Object', () {
+    test('test sort object', () {
+      final Coordinate start = Coordinate(0, 0);
+      final List<MockObject> destinations = [
+        createMockObject(1, 1),
+        createMockObject(5, 3),
+        createMockObject(0, 0),
+        createMockObject(2, 1),
+        createMockObject(0, -1),
+      ];
+
+      final List<MockObject> result = sortObjectByLinearDistance(
+        start,
+        destinations,
+        (MockObject object) => object.value,
       );
 
       expect(result[0], equals(destinations[2]));
