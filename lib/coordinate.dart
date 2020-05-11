f9fe9dfa670cf42d7440b775b84f844496b2d895
WMXPY <wm@mengw.io>
2020-05-11 13:34:51 -0500

    implement cooridate arrage

--- lib/coordinate.dart
+++ lib/coordinate.dart
@@ -2,6 +2,8 @@ library coordinate;
 
 export "./src/arrange/coordinate.dart";
 export "./src/calculate/distance.dart";
+export "./src/calculate/nearest.dart";
 export "./src/calculate/radius.dart";
 export "./src/calculate/range.dart";
+export "./src/entity/declare.dart";
 export "./src/entity/entity.dart";
--- lib/src/arrange/coordinate.dart
+++ lib/src/arrange/coordinate.dart
@@ -1,4 +1,5 @@
 import 'package:coordinate/coordinate.dart';
+import 'package:coordinate/src/entity/declare.dart';
 
 List<Coordinate> arrangeCoordinateByLinearDistance(
     Coordinate start, List<Coordinate> destinations) {
@@ -7,12 +8,55 @@ List<Coordinate> arrangeCoordinateByLinearDistance(
   for (final Coordinate destination in destinations) {
     destinationsSet.add(destination);
   }
-
   final List<Coordinate> result = [];
 
   while (destinationsSet.length > 0) {
     final Coordinate currentStart =
         result.length == 0 ? start : result[result.length - 1];
+    final Coordinate next = getNearestCoordinateByLinearDistance(
+      currentStart,
+      destinationsSet.toList(),
+    );
+
+    if (next != null) {
+      result.add(next);
+      destinationsSet.remove(next);
+    } else {
+      return result;
+    }
+  }
+
+  return result;
+}
+
+List<T> arrangeObjectByLinearDistance<T>(
+  Coordinate start,
+  List<T> objects,
+  GetCoordinateFunction<T> getCoordinateFunction,
+) {
+  final Set<T> objectSet = new Set();
+
+  for (final T object in objects) {
+    objectSet.add(object);
+  }
+  final List<T> result = [];
+
+  while (objects.length > 0) {
+    final Coordinate currentStart = result.length == 0
+        ? start
+        : getCoordinateFunction(result[result.length - 1]);
+    final T next = getNearestObjectByLinearDistance(
+      currentStart,
+      objectSet.toList(),
+      getCoordinateFunction,
+    );
+
+    if (next != null) {
+      result.add(next);
+      objectSet.remove(next);
+    } else {
+      return result;
+    }
   }
 
   return result;
