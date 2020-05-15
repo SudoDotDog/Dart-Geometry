70ac2ecdda964b91172feef6b229e8a7678050e5
WMXPY <wm@mengw.io>
2020-05-15 16:19:57 -0500

    implement numberic parsing

--- lib/src/entity/coordinate.dart
+++ lib/src/entity/coordinate.dart
@@ -1,8 +1,17 @@
+import 'package:numeric/numeric.dart';
+
 class Coordinate {
   final double latitude;
   final double longitude;
 
   Coordinate(this.latitude, this.longitude);
+
+  factory Coordinate.fromLatLngMap(Map<String, dynamic> map) {
+    return Coordinate(
+      tryParseNullDouble(map['lat']),
+      tryParseNullDouble(map['lng']),
+    );
+  }
 
   bool operator ==(dynamic other) {
     return this.equals(other);
